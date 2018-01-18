0639eb89c993f3f01d93d2f5700459ed961cec4e
Edward <edward_song@yeah.net>
2018-01-18 10:31:52 +0800

    1st release

--- PlmBomToTxtBom/Form1.Designer.cs
+++ PlmBomToTxtBom/Form1.Designer.cs
@@ -28,155 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.grbCustomFormat = new System.Windows.Forms.GroupBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnGo = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtExcelFile = new System.Windows.Forms.TextBox();
-            this.chkCustomFormat = new System.Windows.Forms.CheckBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.grbCustomFormat.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.grbCustomFormat);
+            this.groupBox1.Controls.Add(this.btnHelp);
             this.groupBox1.Controls.Add(this.btnGo);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtExcelFile);
-            this.groupBox1.Controls.Add(this.chkCustomFormat);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(516, 200);
+            this.groupBox1.Size = new System.Drawing.Size(608, 63);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // grbCustomFormat
-            // 
-            this.grbCustomFormat.Controls.Add(this.textBox5);
-            this.grbCustomFormat.Controls.Add(this.label7);
-            this.grbCustomFormat.Controls.Add(this.label6);
-            this.grbCustomFormat.Controls.Add(this.label5);
-            this.grbCustomFormat.Controls.Add(this.label4);
-            this.grbCustomFormat.Controls.Add(this.label3);
-            this.grbCustomFormat.Controls.Add(this.label2);
-            this.grbCustomFormat.Controls.Add(this.textBox4);
-            this.grbCustomFormat.Controls.Add(this.textBox3);
-            this.grbCustomFormat.Controls.Add(this.textBox2);
-            this.grbCustomFormat.Location = new System.Drawing.Point(25, 92);
-            this.grbCustomFormat.Name = "grbCustomFormat";
-            this.grbCustomFormat.Size = new System.Drawing.Size(469, 96);
-            this.grbCustomFormat.TabIndex = 4;
-            this.grbCustomFormat.TabStop = false;
-            this.grbCustomFormat.Text = "Custom Format Setting";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(389, 59);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(52, 20);
-            this.textBox5.TabIndex = 11;
-            this.textBox5.Text = "O";
-            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(389, 30);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(55, 13);
-            this.label7.TabIndex = 10;
-            this.label7.Text = "Location";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(339, 30);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(25, 13);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Qty";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(218, 30);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(103, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Part Description";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 62);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Excel Column:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 30);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(121, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Column Description:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(133, 30);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Part Number";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(332, 59);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(42, 20);
-            this.textBox4.TabIndex = 2;
-            this.textBox4.Text = "M";
-            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(221, 59);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(95, 20);
-            this.textBox3.TabIndex = 1;
-            this.textBox3.Text = "D";
-            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(136, 59);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(70, 20);
-            this.textBox2.TabIndex = 0;
-            this.textBox2.Text = "C";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(419, 19);
+            this.btnGo.Location = new System.Drawing.Point(439, 22);
             this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(75, 62);
+            this.btnGo.Size = new System.Drawing.Size(75, 27);
             this.btnGo.TabIndex = 3;
             this.btnGo.Text = "GO";
             this.btnGo.UseVisualStyleBackColor = true;
@@ -185,7 +65,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 29);
+            this.label1.Location = new System.Drawing.Point(11, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 2;
@@ -193,69 +73,75 @@
             // 
             // txtExcelFile
             // 
-            this.txtExcelFile.Location = new System.Drawing.Point(124, 26);
+            this.txtExcelFile.Location = new System.Drawing.Point(108, 26);
             this.txtExcelFile.Name = "txtExcelFile";
-            this.txtExcelFile.Size = new System.Drawing.Size(289, 20);
+            this.txtExcelFile.Size = new System.Drawing.Size(325, 20);
             this.txtExcelFile.TabIndex = 1;
             this.txtExcelFile.DoubleClick += new System.EventHandler(this.txtExcelFile_DoubleClick);
             // 
-            // chkCustomFormat
+            // btnHelp
             // 
-            this.chkCustomFormat.AutoSize = true;
-            this.chkCustomFormat.Location = new System.Drawing.Point(30, 68);
-            this.chkCustomFormat.Name = "chkCustomFormat";
-            this.chkCustomFormat.Size = new System.Drawing.Size(104, 17);
-            this.chkCustomFormat.TabIndex = 0;
-            this.chkCustomFormat.Text = "Custom Format";
-            this.chkCustomFormat.UseVisualStyleBackColor = true;
+            this.btnHelp.Location = new System.Drawing.Point(520, 22);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 27);
+            this.btnHelp.TabIndex = 4;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
             // 
-            // dataGridView1
+            // pictureBox1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 237);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(933, 362);
-            this.dataGridView1.TabIndex = 1;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(608, 246);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(117, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(357, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "請將PLM系統download下來的原始excel綠色方框內容刪除";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1026, 627);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(640, 84);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.grbCustomFormat.ResumeLayout(false);
-            this.grbCustomFormat.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox grbCustomFormat;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button btnGo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtExcelFile;
-        private System.Windows.Forms.CheckBox chkCustomFormat;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PlmBomToTxtBom/Form1.cs
+++ PlmBomToTxtBom/Form1.cs
@@ -28,7 +28,7 @@ namespace PlmBomToTxtBom
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Text = "Wistron PLM Exeel BOM To Text BOM ,Ver:" + System.Windows.Forms.Application.ProductVersion + "(2018-01-18),Author:Edward Song"; 
+            this.Text = "PLM Exeel BOM To Text BOM ,Ver:" + System.Windows.Forms.Application.ProductVersion + ",Author:Edward Song"; 
             //
             createAppFolder();
 
@@ -164,47 +164,140 @@ namespace PlmBomToTxtBom
             {
 
 
-                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
-                Workbooks wkbs = app.Workbooks;
-                _Workbook _wbk = wkbs.Add(txtExcelFile.Text.Trim()); //open the excel file
+              //  Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+              //  Workbooks wkbs = app.Workbooks;
+              //  _Workbook _wbk = wkbs.Add(txtExcelFile.Text.Trim()); //open the excel file
+
+              //  //
+              //  Sheets shs = _wbk.Sheets;
+              //  _Worksheet _wsh = (_Worksheet)shs.get_Item(1);
+              // // _Worksheet _wsh = (_Worksheet)_wbk.Sheets[0];
+
+              //  ((Range)_wsh.Rows[1,Type.Missing ]).Delete(XlDeleteShiftDirection.xlShiftUp);
+              // // _wsh.get_Range(_wsh.Cells[1, 2], _wsh.Cells[_wsh.Rows.Count, 2]).Delete(XlDeleteShiftDirection.xlShiftToLeft);
+              ////  _wsh.get_Range(_wsh.Cells[1, 2],_wsh.Cells[_wsh.Rows.Count, 2]).Delete(XlDeleteShiftDirection.xlShiftToLeft);
+              //  //string ss = _wsh.Range["A1"].Value.ToString();
+              // // MessageBox.Show(ss);
+              //  //MessageBox.Show(_wsh.UsedRange.Columns.Count.ToString());
+              //  //MessageBox.Show(_wsh.UsedRange.Rows.Count.ToString());
+
+              //  string TXTBOM = appFolder + @"\" + txtExcelFile.Text.Trim().Substring(txtExcelFile.Text.Trim().LastIndexOf(@"\") + 1, txtExcelFile.Text.Trim().Length - txtExcelFile.Text.Trim().LastIndexOf(@"\") - 1) + @".txt";
+
+              //  MessageBox.Show(TXTBOM);
+              //  return;
+
+
+
+
+              //  //屏蔽掉系统跳出的Alert
+              //  app.AlertBeforeOverwriting = false;
+              //  //保存到指定目录
+              // // _wbk.SaveAs(appFolder + @"\.1.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+              // //     Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+              //  _wbk.SaveAs(appFolder + @"\1.xlsx");
+
+
+              //  app.Quit();
+              //  //释放掉多余的excel进程
+              //  System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+              //  app = null;
+              //  MessageBox.Show("OK");
+
+
+               //=================
+
+                DataSet ds = new DataSet();
+
+                DataSetParse(txtExcelFile.Text.Trim(), out ds);
+               // dataGridView1.DataSource = ds.Tables[0];
+
+
+
+                //column index
+
+                int _Level = -1; //value = 1,default
+                int _PartNumber = -1;
+                int _PartDescription = -1;
+                int _Qty = -1;
+                int _Location = -1;
+
+
+                //get ColumnName 
+                for (int i = 0; i < ds.Tables[0].Columns.Count; i++)
+                {
+                   // MessageBox.Show(ds.Tables[0].Columns[i].ToString());
+                    if (ds.Tables[0].Columns[i].ToString().ToUpper().Trim() == "LEVEL")
+                        _Level = i;
+                    if (ds.Tables[0].Columns[i].ToString().ToUpper().Trim() == "Part Number".ToUpper ())
+                        _PartNumber  = i;
+                    if (ds.Tables[0].Columns[i].ToString().ToUpper().Trim() == "Part Description".ToUpper())
+                        _PartDescription = i;
+                    if (ds.Tables[0].Columns[i].ToString().ToUpper().Trim() == "Qty".ToUpper())
+                        _Qty = i;
+                    if (ds.Tables[0].Columns[i].ToString().ToUpper().Trim() == "Location".ToUpper())
+                        _Location = i;
+                }                
+
 
                 //
-                Sheets shs = _wbk.Sheets;
-                _Worksheet _wsh = (_Worksheet)shs.get_Item(1);
-               // _Worksheet _wsh = (_Worksheet)_wbk.Sheets[0];
+                string _sLevel = "";
+                string _sPartNumber = "";
+                string _sPartDescription = "";
+                string _sQty = "";
+                string _sLocation = "";
+                string _sLine = "";
 
-                ((Range)_wsh.Rows[1,Type.Missing ]).Delete(XlDeleteShiftDirection.xlShiftUp);
-               // _wsh.get_Range(_wsh.Cells[1, 2], _wsh.Cells[_wsh.Rows.Count, 2]).Delete(XlDeleteShiftDirection.xlShiftToLeft);
-              //  _wsh.get_Range(_wsh.Cells[1, 2],_wsh.Cells[_wsh.Rows.Count, 2]).Delete(XlDeleteShiftDirection.xlShiftToLeft);
-                //string ss = _wsh.Range["A1"].Value.ToString();
-               // MessageBox.Show(ss);
-                //MessageBox.Show(_wsh.UsedRange.Columns.Count.ToString());
-                //MessageBox.Show(_wsh.UsedRange.Rows.Count.ToString());
 
                 string TXTBOM = appFolder + @"\" + txtExcelFile.Text.Trim().Substring(txtExcelFile.Text.Trim().LastIndexOf(@"\") + 1, txtExcelFile.Text.Trim().Length - txtExcelFile.Text.Trim().LastIndexOf(@"\") - 1) + @".txt";
+                
 
-                MessageBox.Show(TXTBOM);
-                return;
-
-
-
-
-                //屏蔽掉系统跳出的Alert
-                app.AlertBeforeOverwriting = false;
-                //保存到指定目录
-               // _wbk.SaveAs(appFolder + @"\.1.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-               //     Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-                _wbk.SaveAs(appFolder + @"\1.xlsx");
+                if (File.Exists(TXTBOM))
+                {
+                    try
+                    {
+                        File.Delete(TXTBOM);
+                    }
+                    catch (Exception ex)
+                    {
 
 
-                app.Quit();
-                //释放掉多余的excel进程
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
-                app = null;
-                MessageBox.Show("OK");
+                    }
+                }
 
-               
 
+                for (int i = 0; i < ds.Tables[0].Columns.Count ; i++)
+                {
+                    for (int j = 0; j < ds.Tables[0].Rows.Count ; j++)
+                    {
+                        _sLevel = ds.Tables[0].Rows[j][_Level].ToString();
+                        _sLocation = ds.Tables[0].Rows[j][_Location].ToString();
+                        if ( _sLevel  == "1"  && !string.IsNullOrEmpty (_sLocation ))
+                        {
+                            _sPartNumber = ds.Tables[0].Rows[j][_PartNumber ].ToString();
+                            _sPartDescription = ds.Tables[0].Rows[j][_PartDescription].ToString();
+                            _sQty = ds.Tables[0].Rows[j][_Qty].ToString();
+                                                     
+                            if (_sLocation.Contains(","))
+                            {
+                                foreach (string  item in _sLocation.Split (','))
+                                {
+                                    _sLine = _sPartNumber.PadRight(36) + _sPartDescription.PadRight(85) + _sQty.PadRight(5) + item;
+                                }
+                            }
+                            else
+                            {
+                                _sLine = _sPartNumber.PadRight(36) + _sPartDescription.PadRight(85) + _sQty.PadRight(5) + _sLocation;
+                            }
+                            StreamWriter sw = new StreamWriter(TXTBOM, true, Encoding.UTF8);
+                            sw.WriteLine(_sLine);
+                            sw.Close();
+                        }
+                    }
+
+                }
+
+
+                MessageBox.Show("Complete!,File save in'" + TXTBOM + "'", "Save OK",MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
@@ -232,6 +325,15 @@ namespace PlmBomToTxtBom
                 MessageBox.Show("you select file is not excel file...", "File Not Excel", MessageBoxButtons.OK, MessageBoxIcon.Stop);
                 return false;
             }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            if (this.Height == 411)
+                this.Height = 122;
+            else
+                this.Height = 411;
+                  
         }
 
 
